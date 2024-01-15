7d6b3f97a9c5e8b87dc332ea4fca9423fde16bbb
Christopher J Mallery <zoggins@retro-spy.com>
2024-01-14 19:25:27 -0800

    Add project files.

---
+++ retrospy_patreon_wordpress_sync/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace retrospy_patreon_wordpress_sync
+{
+    public static class Config
+    {
+        public static string PatreonClientId = string.Empty;
+        public static string PatreonRefreshToken = string.Empty;
+        public static string PatreonClientAccessToken = string.Empty;
+    }
+}
---
+++ retrospy_patreon_wordpress_sync/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using Patreon.Net;
+using Patreon.Net.Models;
+
+namespace retrospy_patreon_wordpress_sync
+{
+
+
+    class Program
+    {
+
+        private static PatreonClient? patreonClient;
+
+        private static bool RefreshingRefreshToken = false;
+
+        private static Task OnTokensRefreshedAsync(OAuthToken token)
+        {
+            RefreshingRefreshToken = true;
+            Environment.SetEnvironmentVariable("PatreonRefreshToken", token.RefreshToken, EnvironmentVariableTarget.User);
+            RefreshingRefreshToken = false;
+            return Task.FromResult(token);
+        }
+
+        private static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+
+
+
+            while (RefreshingRefreshToken) ;
+            patreonClient?.Dispose();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            Config.PatreonClientAccessToken = Environment.GetEnvironmentVariable("PatreonClientAccessToken") ?? string.Empty;
+            Config.PatreonClientId = Environment.GetEnvironmentVariable("PatreonClientId") ?? string.Empty;
+            Config.PatreonRefreshToken = Environment.GetEnvironmentVariable("PatreonRefreshToken") ?? string.Empty;
+
+            patreonClient = new PatreonClient(Config.PatreonClientAccessToken, Config.PatreonRefreshToken, Config.PatreonClientId);
+
+            patreonClient.TokensRefreshedAsync += OnTokensRefreshedAsync;
+        }
+    }
+}
